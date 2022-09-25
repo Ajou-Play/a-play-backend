f12c5cb135d3d5a9fa37f6a2cc9bb6670df63d77
julie0005 <julie0005@ajou.ac.kr>
2022-09-25 12:32:05 +0900

    chore : webconfig 추가

---
+++ src/main/java/com/paran/aplay/common/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.paran.aplay.common.config;
+
+
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  private final ObjectMapper objectMapper;
+
+  @PostConstruct
+  public void initObjectMapper() {
+    objectMapper.getFactory().configure(JsonWriteFeature.ESCAPE_NON_ASCII.mappedFeature(), true);
+    objectMapper.registerModule(new JavaTimeModule());
+  }
+
+  // 프록시 설정 전까지 일단 도메인들 설정.
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/v1/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods(
+            HttpMethod.GET.name(),
+            HttpMethod.HEAD.name(),
+            HttpMethod.POST.name(),
+            HttpMethod.PUT.name(),
+            HttpMethod.DELETE.name(),
+            HttpMethod.PATCH.name())
+        .allowCredentials(true);
+  }
+}
