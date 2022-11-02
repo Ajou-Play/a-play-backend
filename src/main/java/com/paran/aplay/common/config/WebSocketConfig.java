d065a3ed15fbb4875cef372d5545fc74e06b3e15
julie0005 <julie0005@ajou.ac.kr>
2022-11-02 17:21:03 +0900

    feat : submodule 수정

--- src/main/java/com/paran/aplay/common/config/WebSocketConfig.java
+++ src/main/java/com/paran/aplay/common/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint( "/socket").setAllowedOrigins("*")
+    registry.addEndpoint( "/socket").setAllowedOriginPatterns("*")
         .withSockJS();
   }
 }
