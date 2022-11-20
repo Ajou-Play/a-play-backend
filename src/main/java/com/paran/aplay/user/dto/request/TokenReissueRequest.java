e3a6281a222d15887a20b9709d981cc9f3c726ac
julie0005 <julie0005@ajou.ac.kr>
2022-11-21 04:25:03 +0900

    feat : 토큰 재발행 api 구현

---
+++ src/main/java/com/paran/aplay/user/dto/request/TokenReissueRequest.java
@@ -0,0 +1,24 @@
+package com.paran.aplay.user.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenReissueRequest {
+
+    @NotNull
+    private Long userId;
+
+    @NotBlank
+    private String accessToken;
+
+    @NotBlank
+    private String refreshToken;
+
+}
+
