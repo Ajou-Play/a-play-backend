e2ab263190429ee08a469ee3b9b5ef7812285021
julie0005 <julie0005@ajou.ac.kr>
2022-11-27 20:11:32 +0900

    feat : 채널 단건 조회

--- src/main/java/com/paran/aplay/user/repository/UserChannelRepository.java
+++ src/main/java/com/paran/aplay/user/repository/UserChannelRepository.java
@@ -2,8 +2,11 @@ package com.paran.aplay.user.repository;
 
 import com.paran.aplay.user.domain.UserChannel;
 import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserChannelRepository extends JpaRepository<UserChannel, Long> {
   boolean existsByUserIdAndChannelId(@Param("userId") Long userId, @Param("channelId") Long channelId);
+
+  List<UserChannel> findAllByChannelId(@Param("channelId") Long channelId);
 }
