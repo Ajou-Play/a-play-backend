cfaca971957d6e810b8e3bf514f831afc6f0eedc
julie0005 <julie0005@ajou.ac.kr>
2022-11-28 02:31:05 +0900

    fix : 사용되지 않는 테스트 제거

--- src/main/java/com/paran/aplay/user/dto/response/UserDetailResponse.java
+++ src/main/java/com/paran/aplay/user/dto/response/UserDetailResponse.java
@@ -2,13 +2,12 @@ package com.paran.aplay.user.dto.response;
 
 import com.paran.aplay.user.domain.LocalUser;
 import com.paran.aplay.user.domain.User;
-import com.paran.aplay.user.dto.ChatSender;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserDetailResponse {
     private final Long userId;
