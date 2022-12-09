54bb31a2e608d18300358b33fec68ae707eaf5c0
julie0005 <julie0005@ajou.ac.kr>
2022-12-10 04:04:44 +0900

    feat : 문서 저장 api 구현 완료

--- src/main/java/com/paran/aplay/document/controller/DocumentController.java
+++ src/main/java/com/paran/aplay/document/controller/DocumentController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.OK;
 import com.paran.aplay.common.ApiResponse;
 import com.paran.aplay.document.domain.Document;
 import com.paran.aplay.document.dto.request.DocumentCreateRequest;
+import com.paran.aplay.document.dto.request.DocumentUpdateRequest;
 import com.paran.aplay.document.dto.response.DocumentResponse;
 import com.paran.aplay.document.service.DocumentService;
 import java.net.URI;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,11 +39,26 @@ public class DocumentController {
                 .build();
         return ResponseEntity.created(URI.create("/")).body(apiResponse);
     }
+
     @GetMapping("/{documentId}")
     public ResponseEntity<ApiResponse<DocumentResponse>> getDocument(@PathVariable Long documentId) {
         DocumentResponse documentResponse = DocumentResponse.from(documentService.getDocumentById(documentId));
         ApiResponse apiResponse = ApiResponse.builder()
                 .message("문서 단건 조회")
+                .status(OK.value())
+                .data(documentResponse)
+                .build();
+        return ResponseEntity.ok().body(apiResponse);
+    }
+
+    @PatchMapping("/{documentId}")
+    public ResponseEntity<ApiResponse<DocumentResponse>> updateDocument(@PathVariable Long documentId, @RequestBody
+    DocumentUpdateRequest updateRequest) {
+        Document document = documentService.getDocumentById(documentId);
+        Document updatedDocument = documentService.saveDocument(document, updateRequest);
+        DocumentResponse documentResponse = DocumentResponse.from(updatedDocument);
+        ApiResponse apiResponse = ApiResponse.builder()
+                .message("문서 저장 성공")
                 .status(OK.value())
                 .data(documentResponse)
                 .build();
